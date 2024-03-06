b8b9c8cd1ca31e8ab9744f99b11a7c4a2397a8af
Gio Ciudadano <gaciudadano@up.edu.ph>
2024-03-07 01:53:02 +0800

    Add RGB to Lab 3

--- lab03/Ciudadano_lab03_blending.py
+++ lab03/Ciudadano_lab03_blending.py
@@ -34,7 +34,7 @@ def create_gaussian_laplacian_stack(image: np.array):
   gaussian_stack = [image]
   laplacian_stack = []
   for i in range(1, STACK_SIZE+1):
-    gaussian_stack.append(scipy.ndimage.gaussian_filter(gaussian_stack[i-1].astype(np.float32), sigma=16, radius=40))
+    gaussian_stack.append(scipy.ndimage.gaussian_filter(gaussian_stack[i-1].astype(np.float32), sigma=16))
     laplacian_stack.append(gaussian_stack[i-1]-gaussian_stack[i])
   return gaussian_stack, laplacian_stack
 
@@ -46,19 +46,28 @@ def create_blended_image(image_a : np.array, image_b : np.array, mask: np.array)
   blend = []
   for i in range(STACK_SIZE):
     blend.append((laplacian_a[i] * (gaussian_mask[i+1]/255)) + (laplacian_b[i] * (1-(gaussian_mask[i+1]/255))))
-  blended_image = sum(blend)
-  plt.imshow(blended_image, cmap='gray')
+  blended_image = cv2.normalize(sum(blend), None, 0, 1.0,
+cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+  plt.imshow(blended_image)
   plt.show()
 
 selectedOption : str = display_laboratory_details()
 match selectedOption:
   case "1":
     try:
-      image_a = cv2.imread("Ciudadano_lab03_left.png", cv2.IMREAD_GRAYSCALE)
-      image_b = cv2.imread("Ciudadano_lab03_right.png", cv2.IMREAD_GRAYSCALE)
-      mask = cv2.imread("Ciudadano_lab03_verticalmask.png", cv2.IMREAD_GRAYSCALE)
+      image_a = cv2.cvtColor(cv2.imread("Ciudadano_lab03_left.png"), cv2.COLOR_BGR2RGB)
+      image_b = cv2.cvtColor(cv2.imread("Ciudadano_lab03_right.png"), cv2.COLOR_BGR2RGB)
+      mask = cv2.imread("Ciudadano_lab03_verticalmask.png")
     except:
       raise Exception("Could not find 'Ciudadano_lab03_left.png', 'Ciudadano_lab03_right.png', or 'Ciudadano_lab03_verticalmask.png'")
+    create_blended_image(image_a, image_b, mask)
+  case "2":
+    try:
+      image_a = cv2.cvtColor(cv2.imread("Ciudadano_lab03_crazyone.png"), cv2.COLOR_BGR2RGB)
+      image_b = cv2.cvtColor(cv2.imread("Ciudadano_lab03_crazytwo.png"), cv2.COLOR_BGR2RGB)
+      mask = cv2.imread("Ciudadano_lab03_crazymask.png")
+    except:
+      raise Exception("Could not find 'Ciudadano_lab03_crazyone.png', 'Ciudadano_lab03_crazytwo.png', or 'Ciudadano_lab03_crazymask.png'")
     create_blended_image(image_a, image_b, mask)
   case _:
     raise Exception("No option selected. Please run the program again.")
